dd8775039d45d7722f53e2ef4204e01e89dde974
RobinLundSadun <robin.lund.sadun@gmail.com>
2026-05-12 10:04:34 +0200

    Fikser byggfeil fra dekoratør-bump

--- src/utils/amplitude.ts
+++ src/utils/amplitude.ts
@@ -1,5 +1,6 @@
-import { getAmplitudeInstance } from "@navikt/nav-dekoratoren-moduler";
+import { getAnalyticsInstance } from "@navikt/nav-dekoratoren-moduler";
 
-const logger = getAmplitudeInstance("dekoratoren");
+const logger = getAnalyticsInstance("familie-ef-mikrofrontend-minside");
 
-export const logNavigereEvent = () => logger("navigere", { komponent: "familie-ef-mikrofrontend-minside" });
+export const logNavigereEvent = (destinasjon: string) =>
+  logger("navigere", { lenketekst: "familie-ef-mikrofrontend-minside", destinasjon });
